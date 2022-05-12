86b855b15d456eab2375b085c43016241ebd9f7a
Chiara Russo <chiar@MacBook-Pro.attlocal.net>
2022-05-12 11:07:24 -0400

    first commit

---
+++ src/main/java/com/littleitaly/littleitalybe/LittleItalyBeApplication.java
@@ -0,0 +1,13 @@
+package com.littleitaly.littleitalybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleItalyBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittleItalyBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/littleitaly/littleitalybe/LittleItalyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.littleitaly.littleitalybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LittleItalyBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
